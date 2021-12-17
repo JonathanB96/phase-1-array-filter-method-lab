6c7cb2ad6d5aca4a65b3e01aead63535d45559d1
jonathan bola <bolajonathan@yahoo.com>
2021-12-17 16:20:50 -0600

    commit tree 506428ab185aa9f9684dcdbc7815a1214553b342

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(arr, str){
+
+    const flt = arr.filter(name => name.toLowerCase() === str.toLowerCase());
+    return flt;
+
+}
+
+function fuzzyMatch(arr, str){
+    const flt = arr.filter(name => name[0] === str[0]);
+    return flt;
+
+}
+
+function matchName(arr, str){
+    let flt = arr.filter(obj=> obj.name === str);
+    return flt;
+}
